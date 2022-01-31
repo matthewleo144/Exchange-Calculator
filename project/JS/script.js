f98f90a58e0f274900c3fb61e6ff6962b32da740
Matthew Leo <78221078+matthewleo144@users.noreply.github.com>
2022-01-31 10:46:35 +0630

    Update script.js

--- project/JS/script.js
+++ project/JS/script.js
@@ -3,6 +3,7 @@ let to =  document.getElementById("to");
 let input = document.getElementById("input");
 let result = document.getElementById("result");
 let historyList = document.getElementById("historyList");
+let history = document.querySelector("history");
 function createOption(x,y,z){
     let o = document.createElement("option");
     let t = document.createTextNode(y);
@@ -28,24 +29,36 @@ function createTr(x){
     x.map(function(el){
         let td = document.createElement("td");
         let text = document.createTextNode(el);
-        td.appendChild(text);
-        let trashContainer = document.createElement("div");
+        let trashContainer = document.createElement("td");
         let trashIcon = document.createElement("i");
-        trashIcon.classList.add("far","fa-times-circle");
-        trashIcon.addEventListener("click",()=>{
-            const toRemove = document.getElementById(selectedProductContainer.id);
-            toRemove.remove();
-            if(selectedProductAreaTag.children.length===2){
-              buttonTag.remove();
-            }
-          });
+        trashIcon.classList.add("fas", "fa-trash");
+        trashIcon.addEventListener("click", (event) => {
+            tr.remove();
+            localStorage.clear();
+        });
         trashContainer.append(trashIcon);
-        td.appendChild(text);
-        tr.appendChild(td);
-        tr.appendChild(trashContainer);
+        td.append(text);
+        tr.append(td,trashContainer);
     })
-    historyList.appendChild(tr);
+    historyList.append(tr);
 }
+
+//for test
+// function showlist(text){
+//     list.innerHTML += `
+//     <li class="list-Item container add-box animate" id="list${i}">
+//         <div id="listText${i}" class="text">${text}</div>
+//         <div class="controls">
+//             <button class="ho1"><i class="fas fa-edit con1" onclick="editlist(${i})"></i></button>
+//             <button class="ho2"><i class="fas fa-window-close con2" onclick="deletelist(${i})"></i></button>
+//         </div>
+//     </li>`;
+//     i++;
+// }
+
+
+
+
 function store(){
     localStorage.setItem("record",historyList.innerHTML);
 }
@@ -56,7 +69,6 @@ document.getElementById("calc").addEventListener("submit",function(e){
         let x = input.value;
         let y = from.value;
         let z = to.value;
-
         //process
         let fromText = x+" "+from.options[from.selectedIndex].innerText;
         let toText =to.options[to.selectedIndex].innerText;
@@ -80,7 +92,7 @@ document.getElementById("calc").addEventListener("submit",function(e){
     if(localStorage.getItem("record")){
         historyList.innerHTML =localStorage.getItem("record");
     }else{
-        historyList.innerHTML = `<tr id="rowSpacer"><td colspan="4">There is no record</td></tr>`
+        historyList.innerHTML = `<tr id="rowSpacer"><td colspan="5">There is no record</td></tr>`
     }
 })();
 
